b5c53d684cd20a592abf8c35fbe7a0ac0d85d677
gerardpuig <gerardpuig@192.168.1.134>
2021-02-09 18:54:28 +0100

    Modificat a ID autoincrementable

--- M11-Floristeria/src/com/floristeria/model/domain/Floristeria.java
+++ M11-Floristeria/src/com/floristeria/model/domain/Floristeria.java
@@ -5,24 +5,32 @@ import java.util.List;
 
 public class Floristeria {
 
-	List<Producte> productList = new ArrayList<>();
+	private int id=0;
+	
+	private List<Producte> productList = new ArrayList<>();
+	
+	public void addTree(String name, double height, double price) {
 
-	public void addTree(int id, String name, double height, double price) {
-
+		id++;
+		
 		Tree tree = new Tree(id, name, height, price);
 		productList.add(tree);
 
 	}
 
-	public void addFlower(int id, String name, String color, double price) {
+	public void addFlower(String name, String color, double price) {
 
-			Flower flower = new Flower(id, name, color, price);
-			productList.add(flower);
+		id++;
+		
+		Flower flower = new Flower(id, name, color, price);
+		productList.add(flower);
 
 	}
 
-	public void addDecoration(int id, String name, String type, double price) {
+	public void addDecoration(String name, String type, double price) {
 
+		id++;
+		
 		try {
 			Decoration decoration = new Decoration(id, name, type, price);
 			productList.add(decoration);
@@ -38,20 +46,22 @@ public class Floristeria {
 		System.out.println("ARBRES:");
 		
 		productList.stream()
-		.filter(p->p.getClass().getSimpleName().equals("Tree"))
-		.forEach(p->System.out.println(p.toString()));
-		
+			.filter(p->p.getClass().getSimpleName().equals("Tree"))
+			.forEach(p->System.out.println(p.toString()));
+			
 		System.out.println("FLORS:");
-		productList.stream()
 		
-		.filter(p->p.getClass().getSimpleName().equals("Flower"))
-		.forEach(p->System.out.println(p.toString()));
+		productList.stream()
+			.filter(p->p.getClass().getSimpleName().equals("Flower"))
+			.forEach(p->System.out.println(p.toString()));
+		
 		System.out.println("DECORACIÓ:");
 		
 		productList.stream()
-		.filter(p->p.getClass().getSimpleName().equals("Decoration"))
-		.forEach(p->System.out.println(p.toString()));
+			.filter(p->p.getClass().getSimpleName().equals("Decoration"))
+			.forEach(p->System.out.println(p.toString()));
 		
 	}
+
 
 }
