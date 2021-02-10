6ecdb748484e9a5fe80821923cc0ff18cd08354c
Ferran Juanals <ferranjuanals@gmail.com>
2021-02-10 18:55:58 +0100

    Update Controller

--- M11-Floristeria/src/com/floristeria/controller/FloristeriaController.java
+++ M11-Floristeria/src/com/floristeria/controller/FloristeriaController.java
@@ -5,25 +5,35 @@ import com.floristeria.model.domain.Floristeria;
 public class FloristeriaController {
 
 	public Floristeria createFloristeria(String nomFloristeria) {
-		
 		Floristeria floristeria = new Floristeria(nomFloristeria);
-		
 		return floristeria;
 	}
 	
 	public void treeAdd(Floristeria floristeria, String name, double height, double price) {
-		
 		floristeria.addTree(name, height, price);
 	}
 	
 	public void flowerAdd(Floristeria floristeria, String name, String color, double price) {
-		
 		floristeria.addFlower(name, color, price);
 	}
 	
 	public void decorationAdd(Floristeria floristeria, String name, String type, double price) {
-		
 		floristeria.addDecoration(name, type, price);
 	}
 	
+	public void productRemove(Floristeria floristeria, int id) {
+		floristeria.removeProduct(id);
+	}
+	
+	public void getProductStock(Floristeria floristeria, String product) {
+		floristeria.printStock(product);
+	}
+	
+	public void getAllProductsStock(Floristeria floristeria) {
+		floristeria.printStock();
+	}
+	
+	public void getStockValue(Floristeria floristeria) {
+		floristaria.totalStockValue();
+	}
 }
